e1fa5a32e3bb92929f034703a74cd65504490783
Radeon4650 <radeon4650main@gmail.com>
2018-12-02 21:35:36 +0200

    Implemented tree hierarchy with accordion

--- server/routes.py
+++ server/routes.py
@@ -9,9 +9,9 @@ from . import app, Employee
 @app.route('/')
 def main():
     empls = Employee.query.filter(Employee.chief == Employee.id).all()
-    return render_template('table_template.html', employees=empls)
+    return render_template('table_template.html', employees=empls, chief_id='undef')
 
 @app.route('/subordinates/<userid>')
 def get_subordinates(userid):
     subordinates = Employee.query.filter(Employee.chief == userid).filter(Employee.id != userid).all()
-    return render_template('list_element_template.html', employees=subordinates)
+    return render_template('list_element_template.html', employees=subordinates, chief_id=userid)
